d160aa04a87bc3a9fa41813134a060e21d7e2362
Amabel Yeo <amabel.yeo.yj@gmail.com>
2021-03-03 14:59:40 +0000

    Finish user edit, update, index, and destory actions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -11,6 +11,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
   
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
   test "should redirect edit when not logged in" do
     get edit_user_path(@user)
     assert_not flash.empty?
@@ -22,6 +27,16 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
                                               email: @user.email } }
     assert_not flash.empty?
     assert_redirected_to login_url
+  end
+  
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+                                    user: { password:              "password",
+                                            password_confirmation: "password",
+                                            admin: true } }
+    assert_not @other_user.reload.admin?
   end
   
    test "should redirect edit when logged in as wrong user" do
@@ -38,4 +53,5 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert flash.empty?
     assert_redirected_to root_url
   end
+  
 end
