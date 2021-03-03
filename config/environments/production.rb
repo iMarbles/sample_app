0f9d89865003283c07035a80baf0633d68df685a
Amabel Yeo <amabel.yeo.yj@gmail.com>
2021-03-03 16:50:53 +0000

    fix heroku host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'damp-oasis-27474.herokuapp.com'
+  host = 'https://damp-oasis-27474.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
